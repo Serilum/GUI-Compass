355550e9c967655271f7bc5e32a7dd0f5cd9abd6
Rick South <rick@natamus.com>
2023-12-12 10:01:53 +0100

    GUI Compass source code for 1.20.1.

---
+++ Common/src/main/java/com/natamus/guicompass/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.guicompass;
+
+import com.natamus.guicompass.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
---
+++ Fabric/src/main/java/com/natamus/guicompass/ModFabricClient.java
@@ -0,0 +1,19 @@
+package com.natamus.guicompass;
+
+import com.natamus.guicompass.events.GUIEvent;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.gui.GuiGraphics;
+
+public class ModFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() { 
+		registerEvents();
+	}
+	
+	private void registerEvents() {
+		HudRenderCallback.EVENT.register((GuiGraphics guiGraphics, float tickDelta) -> {
+			GUIEvent.renderOverlay(guiGraphics, tickDelta);
+		});
+	}
+}
