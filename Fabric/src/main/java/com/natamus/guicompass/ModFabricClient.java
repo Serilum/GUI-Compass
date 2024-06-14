d73da6d688a49cf8bac74ea76c5c69e0801479ca
Rick South <rick@natamus.com>
2024-06-14 05:29:59 +0200

    GUI Compass source code for 1.21.0.

---
+++ Fabric/src/main/java/com/natamus/guicompass/ModFabricClient.java
@@ -0,0 +1,14 @@
+package com.natamus.guicompass;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() { 
+		registerEvents();
+	}
+	
+	private void registerEvents() {
+
+	}
+}
