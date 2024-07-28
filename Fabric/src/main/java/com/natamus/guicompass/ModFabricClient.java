e44a4bcdbed4ce8b501664247f4aa0fc009fc615
Rick South <rick@natamus.com>
2024-07-28 07:31:31 +0200

    (v4.8) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/guicompass/ModFabricClient.java
+++ Fabric/src/main/java/com/natamus/guicompass/ModFabricClient.java
@@ -1,10 +1,16 @@
 package com.natamus.guicompass;
 
 import net.fabricmc.api.ClientModInitializer;
+import com.natamus.guicompass.util.Reference;
+import com.natamus.collective.check.ShouldLoadCheck;
 
 public class ModFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() { 
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		registerEvents();
 	}
 	
