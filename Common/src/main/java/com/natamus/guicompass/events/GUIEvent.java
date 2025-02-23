9d8890f53d375f9f0056ad23d6c8eb095376eb5b
Rick South <rick@natamus.com>
2025-02-23 11:53:20 +0100

    (v4.9) Improved logic when the GUI elements should be hidden.

--- Common/src/main/java/com/natamus/guicompass/events/GUIEvent.java
+++ Common/src/main/java/com/natamus/guicompass/events/GUIEvent.java
@@ -2,6 +2,7 @@ package com.natamus.guicompass.events;
 
 import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.natamus.collective.functions.GUIFunctions;
 import com.natamus.guicompass.config.ConfigHandler;
 import com.natamus.guicompass.util.Util;
 import net.minecraft.client.Minecraft;
@@ -21,7 +22,7 @@ public class GUIEvent {
 	private static final Minecraft mc = Minecraft.getInstance();
 
 	public static void renderOverlay(GuiGraphics guiGraphics, float tickDelta) {
-		if (mc.options.renderDebug) {
+		if (GUIFunctions.shouldHideGUI()) {
 			return;
 		}
 
